5d8fd3a3d4529bb4d659fa308037d5eb1162829c
saarikoski-jules <jules.saarikoski@gmail.com>
2020-10-03 14:37:08 +0200

    commented working strcpu

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -6,12 +6,9 @@
 ;    By: jsaariko <jsaariko@student.codam.nl>         +#+                      ;
 ;                                                    +#+                       ;
 ;    Created: 2020/10/02 14:34:31 by jsaariko      #+#    #+#                  ;
-;    Updated: 2020/10/02 17:12:27 by jsaariko      ########   odam.nl          ;
+;    Updated: 2020/10/03 14:36:52 by jsaariko      ########   odam.nl          ;
 ;                                                                              ;
 ; **************************************************************************** ;
-
-; section .data <- for declaring consts
-; section .bss <- for declaring variables
 
 section .text
 	global _ft_strcpy
@@ -19,26 +16,18 @@ section .text
 ; copy string into already allocated dest
 ; args: destination, source
 ; returns pointer to dest
-
-; rsi = src (lol)
-; rdi = dest (aaaaaaaa)
 _ft_strcpy:
-	push rcx
-	mov rcx, -1
+	push rcx				; Save rcx just in case
+	mov rcx, -1				; Start counter at -1
 loop:
-	inc rcx
-	mov ah, [rsi + rcx]
-	mov [rdi + rcx], ah
-	cmp ah, 0
-	jne loop
-	pop rcx
-	mov rax, rdi
+	inc rcx					; increment rcx
+	mov ah, [rsi + rcx]		; move character from src to temporary register
+	mov [rdi + rcx], ah		; Move character from tmp register to correct spot on dest
+	cmp ah, 0 				; check if moved character was the string terminator
+	jne loop				; continue looping if not
+	pop rcx					; cleanup
+	mov rax, rdi			; return address of dest
 	ret
 
-; TODO: push and pop ah, or whichever register you wind up using
-
-; use rcx to iterate
-
-; TODO: should I use the ah register or do something else instead?
-
-; You can't move anything from memory to memory, but need to use a temporary register
+; TODO: Should I push and pop ah as well?
+; TODO: Should I use ah as temp register or not? 
