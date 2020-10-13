bee8bd8e2a48a1f15a3545369ced9923e4c9486e
saarikoski-jules <jules.saarikoski@gmail.com>
2020-10-13 17:30:28 +0200

    sort basic setup works as meant to

--- ft_list_sort.s
+++ ft_list_sort.s
@@ -5,21 +5,39 @@ section .text
 ; rsi: sorting function
 
 
+; iterate over each item.
+; if current item and next item are in the wrong order, reorder
+; keep track of swaps
+; if zero swaps occur on a given iteration, you're done
+
+
+; 
+; r10 = first node
+; r9 = second node	
+
+_reorder:
+	ret
+
+_compare:
+	mov rdi, [r10]		; Move value of first node into param
+	mov r9, [r10 + 8]	; store pointer to second node into r9
+	mov rsi, [r9]		; get value of data in r9 as param for function call
+	call rbx			; call function from function pointer
+	ret
+
 _ft_list_sort:
 	mov rbx, rsi		; function pointer now stored in rbx
-	mov rcx, rdi
-	mov rdx, [rcx]		; first value now stored in rdx
-	mov rdi, rdx
+	mov r10, rdi		; head of list now stored in r10
 
-	
-	mov r9, [rcx + 8]
-	cmp r9, 0x0
-	je oops
-	mov rsi, [r9]		; store head->next->data in rsi, works, idk why!
+	push rdi
+	push rsi
+	call _compare
+	cmp rax, 0
+	jle _reorder
+	pop rsi
+	pop rdi
 
-	call rbx
+
 	ret
 
-oops:
-	mov rax, 700
-	ret
+
